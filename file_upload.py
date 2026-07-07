8a923c54fa2c2193962eb0148b86f0c0ca6a45cb
Deekshithpatkar <Dkshith2405@gmail.com>
2026-07-07 14:27:43 +0530

    upload and save multiple file

--- file_upload.py
+++ file_upload.py
@@ -24,3 +24,40 @@ async def upload(file: UploadFile = File()):
     return {
         "size": len(content)
     }
+
+# save uploaded file
+import shutil
+@app.post("/uploadsave")
+def upload(file: UploadFile = File()):
+
+    with open(f"uploads/{file.filename}", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)  #copy from temporary file to new file
+
+    return {
+        "message": "File saved successfully",
+        "filename": file.filename
+    }
+
+
+#multiple file upload
+@app.post("/uploadmultiple")
+def upload(file:list[UploadFile]=File()):
+    return{
+        "total files received":len(file)
+    }
+
+
+#saving multiple files
+@app.post("/uploadsavemultiple")
+def upload(file:list[UploadFile]=File()):
+    for files in file:
+        with open(f"uploads/{files.filename}","wb") as buffer:
+            shutil.copyfileobj(files.file,buffer)
+    return{
+        "message": "all files uploaded",
+        "total files received": len(file)
+    }
+
+
+
+
